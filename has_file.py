3beaa1f6f564292617f2b30f306e8bc069b0f646
levi <iooicoder@gmail.com>
2022-03-02 20:29:29 +0800

    feat: update git commit shell

---
+++ has_file.py
@@ -0,0 +1,73 @@
+import shutil
+import time
+import os
+import pathlib
+
+import fire
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+# Linux
+# desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') 
+# Windows
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+
+print(desktop)
+
+class Watcher:
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self, listen_path):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, listen_path, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Error")
+
+        self.observer.join()
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.exists(d):
+            try:
+                shutil.rmtree(d)
+            except Exception as e:
+                os.unlink(d)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+    #shutil.rmtree(src)
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print("Received created event - %s." % event.src_path)
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print("Received modified event - %s." % event.src_path)
+
+            if "execute.bat" in event.src_path:
+                print("Copy It!")
+                parent = pathlib.Path(event.src_path).parent
+                copytree(parent, desktop)
+
+
+
+if __name__ == '__main__':
+    fire.Fire(Watcher)
---
+++ sh/git_commit_author.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+is_continue() {
+    echo "Do you want to keep doing it?"
+    read -p "Continue by default,Enter your choice [Y/n]: " InstallWhileHostsExist
+
+    case "${InstallWhileHostsExist}" in
+    [yY][eE][sS] | [yY])
+        echo "You will keep doing."
+        ;;
+    [nN][oO] | [nN])
+        echo "exit!"
+        exit 0
+        ;;
+    *)
+        echo "No input, will keep doing."
+        ;;
+    esac
+}
+
+reset_git_commit() {
+    echo "Name: $NAME", "Email: $EMAIL"
+    is_continue
+    git filter-branch --env-filter '
+CORRECT_NAME='"$NAME"'
+CORRECT_EMAIL='"$EMAIL"'
+if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+}
+
+update_git_commit() {
+    echo "Wrong Eamil: $OLD_EMAIL, Name: $NAME", "Email: $EMAIL"
+    is_continue
+    git filter-branch --env-filter '
+OLD_EMAIL='"$OLD_EMAIL"'
+CORRECT_NAME='"$NAME"'
+CORRECT_EMAIL='"$EMAIL"'
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+}
+
+main() {
+    type=$1
+    if [ "$type" = "update" ]; then
+        OLD_EMAIL=$2
+        NAME=$3
+        EMAIL=$4
+
+        if [ ! "$OLD_EMAIL" ]; then
+            echo "please input wrong email."
+            exit 1
+        fi
+    elif [ "$type" = "reset" ]; then
+        NAME=$2
+        EMAIL=$3
+    else
+        echo "update or reset?"
+        exit 1
+    fi
+
+    if [ ! "$NAME" ]; then
+        echo "please input your name"
+        exit 1
+    fi
+
+    if [ ! "$EMAIL" ]; then
+        echo "please input your email"
+        exit 1
+    fi
+
+    if [ "$type" = "update" ]; then
+        update_git_commit
+    elif [ "$type" = "reset" ]; then
+        reset_git_commit
+    fi
+}
+
+main "$@"
