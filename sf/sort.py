aa89b7ffe79dfd93aa77f6e3adda19f077ec60e9
levi <iooicoder@gmail.com>
2022-03-08 19:41:18 +0800

    feat: update sort

--- sf/sort.py
+++ sf/sort.py
@@ -1,12 +1,61 @@
 
 
 DATA = [
-    1, 3, 5, 6, 9, 7, 4, 2, 0, 8
+    1, 3, 5, 7, 6, 4, 2, 8, 9, 0
 ]
 
 
 def swap(data, i, j):
     data[i], data[j] = data[j], data[i]
+
+
+def bubble_sort(data, reverse=False):
+    cnt = len(data)
+    for i in range(cnt-1, 0, -1):
+        for j in range(i):
+            if reverse and data[j] < data[j+1]:
+                swap(data, j, j+1)
+            if not reverse and data[j] > data[j+1]:
+                swap(data, j, j+1)
+    return data
+
+
+# print(bubble_sort(DATA, True))
+
+
+def selection_sort(data, reverse=False):
+    cnt = len(data)
+    for i in range(cnt-1):
+        for j in range(i+1, cnt):
+            if not reverse and data[i] > data[j]:
+                swap(data, i, j)
+            if reverse and data[i] < data[j]:
+                swap(data, i, j)
+    return data
+
+
+# print(selection_sort(DATA,True))
+
+def insert_sort(data, reverse=False):
+    cnt = len(data)
+    for i in range(1, cnt):
+        for j in range(i, 0, -1):
+            if not reverse:
+                if data[j] < data[j-1]:
+                    swap(data, j-1, j)
+                else:
+                    break
+            if reverse:
+                if data[j] > data[j-1]:
+                    swap(data, j-1, j)
+                else:
+                    break
+
+    return data
+
+
+print(insert_sort(DATA, True))
+
 
 
 def quick_sort(data, reverse=False):
