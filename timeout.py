87b3f1b5067a93d9a96f22bc63f57f4a620d9519
levi <iooicoder@gmail.com>
2022-03-28 17:02:00 +0800

    feat: update

--- timeout.py
+++ timeout.py
@@ -1,13 +1,15 @@
-
-
 import threading
-
-print('...部分代码...')
-threading.Event().wait(5)
-print('...剩下的代码...')
+import time
 
 
-import threading
+def trigger_task():
+    pass
+
+
+def user_cancel_task():
+    time.sleep(3)
+    return True
+
 
 class Checker(threading.Thread):
     def __init__(self, event):
@@ -19,9 +21,11 @@ class Checker(threading.Thread):
             print('检查 redis 是否有数据')
             self.event.wait(60)
 
-trigger_task() # 异步任务
+start = time.time()
+trigger_task()  # 异步任务
 event = threading.Event()
 checker = Checker(event)
 checker.start()
-if user_cancel_task(): # 可以马上停止
+if user_cancel_task():  # 可以马上停止
     event.set()
+print('耗时:', time.time() - start)
