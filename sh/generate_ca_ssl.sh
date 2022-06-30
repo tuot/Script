69f356b66c6e59804b2b194ab008758450caf07e
levi <iooicoder@gmail.com>
2022-06-30 14:06:07 +0800

    feat: add generate script for ssl ca

---
+++ sh/generate_ca_ssl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+export MSYS_NO_PATHCONV=1
+
+FILE_NAME="${1}"
+COMMON_NAME="${2}"
+
+openssl req -out "${FILE_NAME}"_ca.pem -new -x509 -nodes -days 3650 \
+    -subj "/C=/ST=/L=/O=/OU=/CN=${COMMON_NAME}/emailAddress="
+mv privkey.pem "${FILE_NAME}"_privkey.pem
+
+echo "00" > "${FILE_NAME}"_index.txt
+openssl genrsa -out "${FILE_NAME}".key 2048
+openssl req -key "${FILE_NAME}".key -new -out "${FILE_NAME}".req \
+    -subj "/C=/ST=/L=/O=/OU=/CN=${COMMON_NAME}App/emailAddress="
+openssl x509 -req -in "${FILE_NAME}".req -CA "${FILE_NAME}"_ca.pem -CAkey "${FILE_NAME}"_privkey.pem -CAserial "${FILE_NAME}"_index.txt -out "${FILE_NAME}".pem -days 3650
