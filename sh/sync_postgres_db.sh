aec2ce4e92278932974c1103f50943bd303a2047
levi <iooicoder@gmail.com>
2022-11-23 17:28:42 +0800

    feat: update sync db script

--- sh/sync_postgres_db.sh
+++ sh/sync_postgres_db.sh
@@ -5,34 +5,49 @@ set -eu
 DB_NAME=bundleb2b-v2.0
 IP_DB_NAME=invoice-portal
 
-if [ "$#" -ne 2 ]; then
-    echo "Please input database Host and Password" >&2
-    exit 1
+ACTION=$1
+DB_USER=postgres
+DB_LIST="$DB_NAME $IP_DB_NAME"
+
+if [ "$ACTION" = "backup" ]; then
+    if [ "$#" -ne 4 ]; then
+        echo "Please input database Host, Port and Password" >&2
+        exit 1
+    fi
+    HOST=$2
+    DB_PORT=$3
+    DB_PASSWORD=$4
 fi
 
-HOST=$1
-DB_USER=postgres
-DB_PASSWORD=$2
-DB_PORT=5433
-
-DB_LIST="${DB_NAME} ${IP_DB_NAME}"
 
 start=$(date "+%s")
-for name in $DB_LIST; do
-    {
-        if [ ! -f "${name}.sql" ]; then
-            DB_URL="postgresql://${DB_USER}:${DB_PASSWORD}@${HOST}:${DB_PORT}/${name}"
-            set -x
-            pg_dump --dbname="${DB_URL}" -f "${name}.sql"
-            set +x
-        fi
+if [ "$ACTION" = "backup" ]; then
+    for name in $DB_LIST; do
+        {
+            db_dump_file="$name.dump"
+            if [ ! -f "$db_dump_file" ]; then
+                DB_URL="postgresql://$DB_USER:$DB_PASSWORD@$HOST:$DB_PORT/$name"
+                set -x
+                # pg_dump --dbname="$DB_URL" -f "$db_dump_file"
+                pg_dump --format=custom --dbname="$DB_URL" -f "$db_dump_file"
+                set +x
+            fi
+        } &
+    done
+    wait
+elif [ "$ACTION" = "restore" ]; then
+    for name in $DB_LIST; do
+        {
+            db_dump_file="$name.dump"
+            psql -U postgres -c "drop database IF EXISTS \"$name\""
+            psql -U postgres -c "create database \"$name\""
+            # psql -U postgres -d "$name" -f "$db_dump_file" 
+            pg_restore -U postgres -v --format=custom -d "$name" "$db_dump_file"
+            rm "$db_dump_file"
+        } &
+    done
+    wait
+fi
 
-        psql -U postgres -c "drop database IF EXISTS \"${name}\""
-        psql -U postgres -c "create database \"${name}\""
-        psql -U postgres -d "${name}" -f "${name}.sql"
-        rm "${name}.sql"
-    } &
-done
-wait
 end=$(date "+%s")
 echo "It takes $((end - start)) seconds to complete this task..."
